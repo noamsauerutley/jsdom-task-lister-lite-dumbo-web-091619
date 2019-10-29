011e88ad7d76425198f09df64ea59a23fcf4a95d
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-10-29 11:24:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log("loaded")
+  document.querySelector('input[type="submit"]').addEventListener("click", function(event){
+    let newTask = document.getElementById("new-task-description").value
+    let ul = document.getElementById("tasks")
+    let li = document.createElement("li")
+    li.appendChild(document.createTextNode(newTask))
+    ul.appendChild(li)
+    event.preventDefault()
+  })
+  
 });
